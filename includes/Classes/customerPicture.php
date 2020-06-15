27cfacd066886647f2d0ff05db2f19d96d877042
Enis <enis.trevisi@gmail.com>
2020-06-16 00:45:01 +0200

    initial yourAccount  classes structure

---
+++ includes/Classes/customerPicture.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * This source file is subject to the GNU General Public License v3.0
+ * that is bundled with this package in the file license.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.gnu.org/licenses/gpl-3.0.html
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@skyverge.com so we can send you a copy immediately.
+ *
+ * @author    Enis Trevisi
+ * @copyright Copyright (c) 2020, Enis Trevisi
+ * @license   http://www.gnu.org/licenses/gpl-3.0.html GNU General Public License v3.0
+ * @package   Your Photos
+ */
+
+namespace YourPhotos\Classes;
+
+/** Single instance of a customer picture */
+class CustomerPicture {
+
+	}
+
+	/**
+	 * Deletes the picture
+	 *
+	 * @since 1.0
+	 */
+	public function delete() {
+
+		// deleting files or not?
+
+	}
+
+	/**
+	 * Replaces the file
+	 *
+	 * @since 1.0
+	 */
+	public function replace_file( $new_file ) {
+		// TODO
+	}
+}
