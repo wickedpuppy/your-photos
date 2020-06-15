ef0b782b7e6d9fdaa4010ce227478ad1407e456b
Enis <enis.trevisi@gmail.com>
2020-06-16 00:18:00 +0200

    initial api class structure

---
+++ includes/Classes/api.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * This source file is subject to the GNU General Public License v3.0
+ * that is bundled with this package in the file license.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.gnu.org/licenses/gpl-3.0.html
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@skyverge.com so we can send you a copy immediately.
+ *
+ * @author    Enis Trevisi
+ * @copyright Copyright (c) 2020, Enis Trevisi
+ * @license   http://www.gnu.org/licenses/gpl-3.0.html GNU General Public License v3.0
+ * @package   Your Photos
+ */
+
+namespace YourPhotos\Classes;
+
+/**
+ * Takes care of the custom api endpoints
+ */
+class Api {
+
+	/** GET */
+	public function read( $id ) {
+	}
+}
